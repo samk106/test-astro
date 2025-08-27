2fb88fadddad9dc0f1bb1ca69f13b94d7b028dff
samk106 <sameerku106@gmail.com>
2025-08-27 08:57:24 +0530

    1st

---
+++ src/site.config.mjs
@@ -0,0 +1,43 @@
+export default {
+    siteTitle: "The Storyteller", // Main site title displayed in header
+    siteSubTitle: "Minimal musings on code, design, and life", // Subtitle shown under main title
+    copyright: "© 2025 The Storyteller. All Rights Reserved.", // Footer copyright text
+    showAuthorsOnHomePage: false, // Display author info on homepage
+    showFeaturrdPostsOnHomePage: true, // Show featured posts section on homepage
+    showCategoryOnPosts: true, // Display categories on posts in homepage
+    labels: {
+        featuredPosts: "Featured Posts", // Title for featured posts section
+        latestPosts: "Latest Posts", // Title for latest posts section
+        viewAllPosts: "View All Posts", // Text for link to paginated blog
+        backToHome: "Back to Home", // Back navigation text
+        youMightAlsoLike: "You Might Also Like", // Similar posts section title
+        postedIn: "Posted in", // Category prefix text in articles
+        noArticlesFound: "No articles found.", // Empty state message
+        allCategories: "All Categories", // Categories page title
+        allTags: "All Tags", // Tags page title
+        allAuthors: "All Authors", // Authors page title
+        exploreArticlesByTags: "Explore articles organized by topics", // Tags page description
+        exploreArticlesByCategories: "Explore articles organized by topics", // Categories page description
+        exploreArticlesByAuthors: "Explore articles organized by authors", // Authors page description
+        readMore: "Read More", // Text for "Read More" links on featured posts
+        shareThisArticle: "Share this article", // Share button text
+    },
+    pagination: {
+        showPrevNext: true, // Show Previous/Next navigation buttons
+        prevText: "Previous", // Text for previous page button
+        nextText: "Next", // Text for next page button
+        postLabel: "posts", // Label used in pagination info (e.g., "8 posts")
+    },
+    defaultAuthorName: "Hasin Hayder",
+    showCategoriesLinkOnFooter: true, // Show Categories link in footer
+    showTagsLinkOnFooter: true, // Show Tags link in footer
+    showAuthorsLinkOnFooter: true, // Show Authors link in footer
+    showSimilarPosts: true, // Display similar posts on article pages
+    showReadMoreLinkOnFeaturedPosts: true, // Show "Read More" on featured cards
+    showThumbnailOnFeaturedPosts: true, // Display thumbnails on featured posts
+    numberOfLatestPostsOnHomePage: 6, // Number of latest posts on homepage
+    numberOfBlogPostsPerPage: 8, // Number of posts per paginated blog
+
+    gTag: "G-V5QHDKBFP" // Google Analytics tracking ID
+}
+
